a159e31be854912bf602e283074b899fb178746d
Osayuk1 <157780677+Osayuk1@users.noreply.github.com>
2025-06-04 10:17:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ import random
 from modules.sleep.main import goto_sleep
 from modules.logging.main import write_log
 
+import threading
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "OK"
+
+def run_server():
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
+
+# Start the server in a background thread
+threading.Thread(target=run_server).start()
 class RedditBot:
     def __init__(self, account, proxy=None) -> None:
         self.username = account['username']
